36f93d94c13366e1c684258ec26982b632692cfc
soyeonee <busanpsy1@gmail.com>
2023-05-05 10:39:20 -0400

    Classes: enums

--- main.dart
+++ main.dart
@@ -1,8 +1,33 @@
-class Player {
-  final String name;
-  int xp;
+enum Team { red, blue }
 
-  Player(this.name, this.xp);
+enum XPLevel { beginner, medium, pro }
+
+class Player {
+  String name;
+  Team team;
+  XPLevel xp;
+
+  // Player.fromJson(Map<String, dynamic> playerJson)
+  //     : name = playerJson['name'],
+  //       xp = playerJson['xp'],
+  //       team = playerJson['team'];
+
+  Player(
+      {required this.name,
+      required this.xp,
+      required this.team}); //position is improtant
+
+  // Player.createBluePlayer({required String name, required int age})
+  //     : this.age = age,
+  //       this.name = name,
+  //       this.team = 'blue',
+  //       this.xp = 0;
+
+  // Player.createRedPlayer(String name, int age) //positional parameter
+  //     : this.age = age,
+  //       this.name = name,
+  //       this.team = 'red',
+  //       this.xp = 0;
 
   void sayHello() {
     print("Hello my name is $name, $xp");
@@ -10,8 +35,38 @@ class Player {
 }
 
 void main() {
-  var player = Player("soyeon", 2600);
-  player.sayHello();
-  var player2 = Player("sunny", 3000);
-  player2.sayHello();
+  // var player = Player.createBluePlayer(
+  //   name: "soyeon",
+  //   age: 21,
+  // );
+  // var redPlayer = Player.createRedPlayer("sunny", 20);
+  // var apiData = [
+  //   {
+  //     "name": "soyeon",
+  //     "team": "red",
+  //     "xp": 0,
+  //   },
+  //   {
+  //     "name": "sunny",
+  //     "team": "red",
+  //     "xp": 0,
+  //   },
+  //   {
+  //     "name": "sunmin",
+  //     "team": "red",
+  //     "xp": 0,
+  //   },
+  // ];
+
+  // apiData.forEach((playerJson) {
+  //   var player = Player.fromJson(playerJson);
+  //   player.sayHello();
+  // });
+
+  var soyeon = Player(name: 'soyeon', xp: XPLevel.medium, team: Team.blue);
+  var potato = soyeon
+    ..name = 'ast'
+    ..xp = XPLevel.beginner
+    ..team = Team.red
+    ..sayHello();
 }
